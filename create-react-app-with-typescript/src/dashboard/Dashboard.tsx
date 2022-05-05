ccd58d2519f6cea460425031158fc65a499b6e86
Jenova,Inc <100060839+JENOVA-SYSTEMS@users.noreply.github.com>
2022-05-05 16:30:03 +0900

    prototype

--- create-react-app-with-typescript/src/dashboard/Dashboard.tsx
+++ create-react-app-with-typescript/src/dashboard/Dashboard.tsx
@@ -18,16 +18,16 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
-import Chart from './Chart';
-import Deposits from './Deposits';
-import Orders from './Orders';
+import LineWorks from './LineWorks';
+import Notion from './Notion';
+import Kintone from './kintone';
 
 function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        JENOVA
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -122,7 +122,7 @@ function DashboardContent() {
               noWrap
               sx={{ flexGrow: 1 }}
             >
-              Dashboard
+              JENOVA
             </Typography>
             <IconButton color="inherit">
               <Badge badgeContent={4} color="secondary">
@@ -166,36 +166,45 @@ function DashboardContent() {
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
-              {/* Chart */}
+              {/* LineWorks */}
               <Grid item xs={12} md={8} lg={9}>
                 <Paper
                   sx={{
                     p: 2,
                     display: 'flex',
                     flexDirection: 'column',
-                    height: 240,
+                    height: 300,
                   }}
                 >
-                  <Chart />
+                {/*Chart→LineWorks*/}
+                  <LineWorks />
                 </Paper>
               </Grid>
-              {/* Recent Deposits */}
+              {/* 通知 */}
               <Grid item xs={12} md={4} lg={3}>
                 <Paper
                   sx={{
                     p: 2,
                     display: 'flex',
                     flexDirection: 'column',
-                    height: 240,
+                    height: 300,
                   }}
                 >
-                  <Deposits />
+                {/*Deposits→Notion*/}
+                  <Notion />
                 </Paper>
               </Grid>
-              {/* Recent Orders */}
+              {/*　kintone */}
               <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Orders />
+                <Paper sx={{
+                  p: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  height: 300,
+                  }}
+                >
+                  {/*Orders→kintone*/}
+                  <Kintone />
                 </Paper>
               </Grid>
             </Grid>
